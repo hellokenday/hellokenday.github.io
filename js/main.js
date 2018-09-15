2ead315e36e9551850d0d2fff01338fcfefcb4e0
Ken <hellokenday@gmail.com>
2018-09-15 13:34:45 +0100

    cupdate from portfolio repo

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
 $( document ).ready(function() {
   
+  fadeInPages();
   pauseCarousels();
+  hideNavbarOnScroll();
+  
+  window.LazyLoad = new LazyLoad({
+    elements_selector: ".lazy-load",
+  });
+  window.LazyLoad.update();
 });
+
+function fadeInPages() {
+
+ $('body').fadeIn(700).removeClass('hidden');
+}
 
 // Bootstrap carousel
 function pauseCarousels() {
   
-  $('.carousel').carousel({   
-    interval: false 
-  });
+  // check if carousel exists
+  if ( $('.carousel').length ) {
+
+    // pause carousels
+    $('.carousel').carousel({   
+      interval: false 
+    });
+  }
+}
+
+// Hide nav on scroll down and show on scroll up
+function hideNavbarOnScroll() {
+  
+  $(".navbar.sticky-top").autoHidingNavbar();
 }
 
 // Lazyload images
 (function(w, d){
 	var b = d.getElementsByTagName('body')[0];
 	var s = d.createElement("script"); s.async = true;
-	var v = !("IntersectionObserver" in w) ? "8.9.0" : "10.8.0";
+	var v = !("IntersectionObserver" in w) ? "8.15.2" : "10.17.0";
 	s.src = "https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/" + v + "/lazyload.min.js";
 	w.lazyLoadOptions = {
-    elements_selector: ".lazy-load"
+    elements_selector: ".lazy-load",
   }; // Your options here. See "recipes" for more information about async.
 	b.appendChild(s);
 }(window, document));
